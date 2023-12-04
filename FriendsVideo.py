e75210b058c2b5b278d976d6abfcd59b00a34f80
sharma-coder2008 <140511396+sharma-coder2008@users.noreply.github.com>
2023-12-04 22:52:04 +0530

    Add files via upload

---
+++ FriendsVideo.py
@@ -0,0 +1,38 @@
+import os
+import cv2
+
+
+path = "Images1/"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.png', '.jpg','.avif','.webp','.jpeg']:
+        file_name = path+"/"+file
+        print(file_name)
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size = (width,height)
+print(size)
+
+
+out = cv2.VideoWriter('friend.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+
+#For SUNSET
+for i in range(1,count-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+    
+    
+out.release() # releasing the video generated
+print("Done")
+
+
